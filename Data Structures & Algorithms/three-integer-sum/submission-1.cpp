3d05b97e7983bce89ea0a2216044928fd03de2ee
Siya Singh <92603603+siya0in@users.noreply.github.com>
2026-07-19 07:00:37 -0700

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        if (arr.size() < 3) return {};
+
+        // Sort the elements
+        sort(arr.begin(), arr.end());
+        set<vector<int>> result;  // set auto-removes duplicate triplets
+
+        // Now fix the first element and find the other two elements
+        for (int i = 0; i < (int)arr.size() - 2; i++) {
+
+            // Find other two elements using Two-Pointer approach
+            int left = i + 1;
+            int right = arr.size() - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    // Add the triplet, and move to find other triplets
+                    result.insert({arr[i], arr[left], arr[right]});
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return vector<vector<int>>(result.begin(), result.end());
+    }
+};
