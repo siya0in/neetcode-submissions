628041c18ee861ef90c0181dbc9df4ae3190340b
Siya Singh <92603603+siya0in@users.noreply.github.com>
2026-07-27 15:25:44 -0700

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(std::string s, int k) {
+        int res=0;
+        unordered_set<char> hash(s.begin(),s.end());
+        for(char c:s){
+            int maxf=0;int l=0;
+            for(int r=0;r<s.size();r++){
+                if(s[r]==c){
+                    maxf++;
+                }
+                while((r-l+1)-maxf > k){
+                    if(s[l]==c){
+                        maxf--;
+                    }
+                    l++;
+                }
+                res=max(res,r-l+1); 
+            }
+        }
+        return res;
+    }
+};
