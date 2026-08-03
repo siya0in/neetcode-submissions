a24a045ec4f0c6dd452c1dfde4b774d19480e777
Siya Singh <92603603+siya0in@users.noreply.github.com>
2026-08-02 19:11:06 -0700

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> arr;
+        dfs(root, arr);
+        return arr[k - 1];
+    }
+
+    void dfs(TreeNode* node, vector<int>& arr) {
+        if (!node) return;
+        dfs(node->left, arr);
+        arr.push_back(node->val);
+        dfs(node->right, arr);
+    }
+};
